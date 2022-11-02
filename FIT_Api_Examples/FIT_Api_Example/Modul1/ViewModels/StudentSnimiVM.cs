058b985504959aab60294714bf5838031f6a62a2
Adil <adil@fit.ba>
2022-11-02 10:08:08 +0100

    ok

--- FIT_Api_Examples/FIT_Api_Example/Modul1/ViewModels/StudentSnimiVM.cs
+++ FIT_Api_Examples/FIT_Api_Example/Modul1/ViewModels/StudentSnimiVM.cs
@@ -1,11 +1,12 @@
 ﻿namespace FIT_Api_Example.Modul2.ViewModels
 {
-    public class StudentUpdateVM
+    public class StudentSnimiVM
     {
         public string ime { get; set; }
         public string prezime { get; set; }
         public string broj_indeksa { get; set; }
         public DateTime datum_rodjenja { get; set; }
         public int opstina_rodjenja_id { get; set; }
+        public int id { get; set; }
     }
 }
