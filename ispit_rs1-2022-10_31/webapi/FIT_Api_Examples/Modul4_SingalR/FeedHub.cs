2a646367fcae18c15a55591c5136488e24931b39
Adil <adil@fit.ba>
2022-12-21 00:45:05 +0100

    ok

--- ispit_rs1-2022-10_31/webapi/FIT_Api_Examples/Modul4_SingalR/FeedHub.cs
+++ ispit_rs1-2022-10_31/webapi/FIT_Api_Examples/Modul4_SingalR/FeedHub.cs
@@ -10,16 +10,6 @@
 
         }
 
-        public async Task Posalji1(string podaci)
-        {
-            // Call the broadcastMessage method to update clients.
-            await Clients.All.SendAsync("preuzmi_poruku_od_server1", podaci);
-        }
-
-        public async Task Posalji2(string podaci)
-        {
-            // Call the broadcastMessage method to update clients.
-            await Clients.All.SendAsync("preuzmi_poruku_od_server2", podaci);
-        }
+  
     }
 }
